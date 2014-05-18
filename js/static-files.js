8d814a9b8ec82326590d7b6b04637ca782bcfc57
apeyrard <augustin.peyrard@gmail.com>
2014-05-18 02:13:16 +0200

    Initialize some routes (introduce angular's ngRoute).
    Add a static 404 route to /error/404
    Add two routes for files, one without syntax, and one with syntax.
    Retrieve syntax and filename from URL and display them.

--- js/static-files.js
+++ js/static-files.js
@@ -5,12 +5,48 @@
     /**
      * static-files module.
      */
-    var staticFiles = angular.module('static-files', []);
+    var staticFiles = angular.module('static-files', ['ngRoute']);
+
+    /**
+     * Routes configuration
+     */
+    staticFiles.config(function ($routeProvider) {
+        $routeProvider.when('/error/404', {
+            templateUrl: 'partials/404.html',
+        });
+        $routeProvider.when('/:file', {
+            templateUrl: 'partials/static-file.html',
+            controller: 'StaticFileCtrl'
+        });
+        $routeProvider.when('/:syntax/:file', {
+            templateUrl: 'partials/static-file.html',
+            controller: 'StaticFileCtrl'
+        });
+        $routeProvider.otherwise({
+            redirectTo: '/error/404'
+        });
+    });
+
 
     /**
      * The Main controller.
      */
     staticFiles.controller('MainCtrl', function ($scope) {
         $scope.title = 'Static files';
+
+        $scope.setTitle = function(title) {
+            $scope.title = title;
+        }
     });
+
+    /**
+     * The StaticFile controller.
+     */
+    staticFiles.controller('StaticFileCtrl', function ($scope, $routeParams) {
+        $scope.filename = $routeParams.file;
+        $scope.syntax = $routeParams.syntax || 'not defined';
+
+        $scope.setTitle($scope.filename);
+    });
+
 })();
